75beebae49e19a4c717ca254dd47f3bbf9ed4bd2
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-05 22:45:39 +0800

    refactor: add hero and meta tags

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -1,24 +1,18 @@
+import { HeroSection } from "@features/landing"
 import { generatedMetadata } from "@shared/libs"
 
 export function meta() {
   return generatedMetadata({
-    title: "YOUR_TITLE | YOUR_SITE",
-    description: "YOUR_SITE_DESCRIPTION",
+    title: "Level Your Devs Skills | The Scolon",
+    description:
+      "Top place to learn and growing development skills, career, business and learn to become better developers.",
   })
 }
 
 export default function HomePage() {
   return (
     <main className="flex flex-col gap-20">
-      <div className="flex flex-col text-center justify-center">
-        <p className="font-mono tracking-tight">__Starter site__</p>
-        <p className="mt-6 leading-relaxed text-foreground/60">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-          repellat qui vitae laboriosam. A, quae nostrum. Ut aut dicta deleniti
-          praesentium nesciunt, laboriosam, mollitia, exercitationem vitae
-          asperiores reprehenderit ullam repellat.
-        </p>
-      </div>
+      <HeroSection />
     </main>
   )
 }
